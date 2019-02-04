6e4534c4a9784e9b2a8cba0f7b57bf37af138456
Bletty Mathew <blettymathew@gmail.com>
2019-02-04 14:38:45 -0500

    First commit

--- RunnerGameApp/GameScene.swift
+++ RunnerGameApp/GameScene.swift
@@ -9,81 +9,92 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+
+class GameScene: SKScene , SKPhysicsContactDelegate
+
+{
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
+    var cat : CatNode!
+    var platforms: Array<SKSpriteNode>?
+    
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let backgroundTexture = SKTexture(image: #imageLiteral(resourceName: "background"))
+        let backgroundNode = SKSpriteNode(texture: backgroundTexture)
+        backgroundNode.size = CGSize(width: 1334, height: 750)
+        self.addChild(backgroundNode)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let catTexture = SKTexture(image: #imageLiteral(resourceName: "cat_run_1"))
+        let catSize  = CGSize (width: 140, height : 140)
+        self.cat = CatNode(texture: catTexture, color: UIColor.white, size: catSize)
+        self.cat.position = CGPoint(x: -520, y: -200)
+        self.addChild(self.cat)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        self.cat.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 70, height: 140))
+        
+        self.platforms = Array<SKSpriteNode>()
+        
+        for i in 0 ..< 10
+        
+        {
+        
+            let platformX = (-440 + (i * 400))
+            let platform = SKSpriteNode(texture: SKTexture(image: #imageLiteral(resourceName: "platform")))
+            platform.size = CGSize(width: 256, height: 64)
+            platform.position = CGPoint(x: platformX, y: -232)
+            platform.physicsBody = SKPhysicsBody(rectangleOf : platform.size)
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            platform.physicsBody?.isDynamic = false
+            
+            platform.physicsBody?.contactTestBitMask = 1
+            
+            self.platforms?.append(platform)
+            self.addChild(platform)
+            
+            
+           // let moveAction = SKAction
+            
+            let rainEmitter = SKEmitterNode(fileNamed: "SnowParticle")
+            rainEmitter?.position = CGPoint(x: 0, y: 400)
+            self.addChild(rainEmitter!)
+            
+           // self.physicsWorld.contactDelegate = self
+
+        
+        
         }
+        
     }
+
+func didBegin(_ contact: SKPhysicsContact)
+{
+    let cat = (contact.bodyA.node as? CatNode) ?? (contact.bodyB.node as? CatNode)
+    
+    if cat?.isJumping ?? false
+    {	cat?.startRunAnimation()
+    }
+}
+
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+       
+        
+        self.cat.startJumpAnimation()
+        cat.jump()
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+       
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
 }
